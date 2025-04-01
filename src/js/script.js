89161418da1377713c5454c2741baaf4a6102b63
Wellington <welltecnic@hotmail.com>
2025-04-01 09:10:00 -0300

    declaração e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome ="Cidade";
+console.log(sobrenome)
+//undefined
+let aula;
+console.log(aula)
+//null
+let valor = null;
+console.log(valor)
+
+let exemplo1={};
+
+let exemplo2=[];

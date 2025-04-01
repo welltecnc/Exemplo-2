8c68fd4bf4c5282e01f14e95a77c740c6ce13926
Wellington <welltecnic@hotmail.com>
2025-04-01 09:16:16 -0300

    adicionando tipos de variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,19 @@ console.log(valor)
 let exemplo1={};
 
 let exemplo2=[];
+
+//TIPOS VARIAVEIS
+let exemplo3 = 10;
+console.log(typeof exemplo3)
+
+let exemplo4="Aula"
+console.log(typeof exemplo4)
+
+let exemplo5 =true;
+console.log(typeof exemplo5)
+
+let exemplo6 =["huguinho","zezinho","luizinho"];
+console.log(typeof exemplo6)
+
+//CONVERSÕES 
+

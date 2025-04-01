da9d143c3735f30d009dcbf34f7d449a71cd4226
Wellington <welltecnic@hotmail.com>
2025-04-01 09:27:32 -0300

    adicionando conversões

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,52 @@
-//DECLARAÇÕES E VARIAVEIS
-var nome ="fiap";
-console.log(nome)
+// //DECLARAÇÕES E VARIAVEIS
+// var nome ="fiap";
+// console.log(nome)
 
-let idade =19;
-console.log(idade)
+// let idade =19;
+// console.log(idade)
 
-const sobrenome ="Cidade";
-console.log(sobrenome)
-//undefined
-let aula;
-console.log(aula)
-//null
-let valor = null;
-console.log(valor)
+// const sobrenome ="Cidade";
+// console.log(sobrenome)
+// //undefined
+// let aula;
+// console.log(aula)
+// //null
+// let valor = null;
+// console.log(valor)
 
-let exemplo1={};
+// let exemplo1={};
 
-let exemplo2=[];
+// let exemplo2=[];
 
-//TIPOS VARIAVEIS
-let exemplo3 = 10;
-console.log(typeof exemplo3)
+// //TIPOS VARIAVEIS
+// let exemplo3 = 10;
+// console.log(typeof exemplo3)
 
-let exemplo4="Aula"
-console.log(typeof exemplo4)
+// let exemplo4="Aula"
+// console.log(typeof exemplo4)
 
-let exemplo5 =true;
-console.log(typeof exemplo5)
+// let exemplo5 =true;
+// console.log(typeof exemplo5)
 
-let exemplo6 =["huguinho","zezinho","luizinho"];
-console.log(typeof exemplo6)
+// let exemplo6 =["huguinho","zezinho","luizinho"];
+// console.log(typeof exemplo6)
 
 //CONVERSÕES 
+
+//float => inteiro
+let numFloat =123.456;
+console.log(parseInt(numFloat));
+
+//string => float
+let numString ="547.987";
+console.log(parseFloat(numString))
+
+//float => string
+
+let numFloat1 =554.665;
+console.log(numFloat1.toString());
+
+let numInt=100;
+console.log(numInt.toString())
+
 

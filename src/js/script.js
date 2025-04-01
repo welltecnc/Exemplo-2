20e2c1fb59de4c3c30909689a6b7e7baa2851965
Wellington <welltecnic@hotmail.com>
2025-04-01 09:49:59 -0300

    adcionando operadores aritmeticos e logicos

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,98 @@
-// //DECLARAÇÕES E VARIAVEIS
-// var nome ="fiap";
-// console.log(nome)
+// // //DECLARAÇÕES E VARIAVEIS
+// // var nome ="fiap";
+// // console.log(nome)
 
-// let idade =19;
-// console.log(idade)
+// // let idade =19;
+// // console.log(idade)
 
-// const sobrenome ="Cidade";
-// console.log(sobrenome)
-// //undefined
-// let aula;
-// console.log(aula)
-// //null
-// let valor = null;
-// console.log(valor)
+// // const sobrenome ="Cidade";
+// // console.log(sobrenome)
+// // //undefined
+// // let aula;
+// // console.log(aula)
+// // //null
+// // let valor = null;
+// // console.log(valor)
 
-// let exemplo1={};
+// // let exemplo1={};
 
-// let exemplo2=[];
+// // let exemplo2=[];
 
-// //TIPOS VARIAVEIS
-// let exemplo3 = 10;
-// console.log(typeof exemplo3)
+// // //TIPOS VARIAVEIS
+// // let exemplo3 = 10;
+// // console.log(typeof exemplo3)
 
-// let exemplo4="Aula"
-// console.log(typeof exemplo4)
+// // let exemplo4="Aula"
+// // console.log(typeof exemplo4)
 
-// let exemplo5 =true;
-// console.log(typeof exemplo5)
+// // let exemplo5 =true;
+// // console.log(typeof exemplo5)
 
-// let exemplo6 =["huguinho","zezinho","luizinho"];
-// console.log(typeof exemplo6)
+// // let exemplo6 =["huguinho","zezinho","luizinho"];
+// // console.log(typeof exemplo6)
 
-//CONVERSÕES 
+// //CONVERSÕES 
 
-//float => inteiro
-let numFloat =123.456;
-console.log(parseInt(numFloat));
+// //float => inteiro
+// let numFloat =123.456;
+// console.log(parseInt(numFloat));
 
-//string => float
-let numString ="547.987";
-console.log(parseFloat(numString))
+// //string => float
+// let numString ="547.987";
+// console.log(parseFloat(numString))
 
-//float => string
+// //float => string
 
-let numFloat1 =554.665;
-console.log(numFloat1.toString());
+// let numFloat1 =554.665;
+// console.log(numFloat1.toString());
 
-let numInt=100;
-console.log(numInt.toString())
+// let numInt=100;
+// console.log(numInt.toString())
+
+// //METODOS - PARTE-1
+
+// //LENGTH- VERIFICA O TAMANHO DA STRING
+
+// let frase ="O mundo da Tecnologia";
+// console.log(frase.length)
+
+// //indexOf -retorna um trecho de um texto
+
+// let texto = "Programação Sustentável";
+// console.log(texto.indexOf("a"))
+
+// //slice - retorna parte de um texto apontando o inicio e o final 
+
+// let info="processamento de ponta";
+// console.log(info.slice(0,13))
+
+//OPERADORES ARITMÉTICOS
+
+const num1 =10;
+const num2 =20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+
+//OPERADORES LÓGICOS 
+
+const num3 =20;
+const num4 =30;
+
+console.log(num3 < num4);
+console.log(num3 < num4 && num4 < 50);
+console.log(num3 > num4 || num4 > num3)
+console.log(num4 == num3 || num4 > num3)
+
+
+
+
+
+
+
+
 
 
